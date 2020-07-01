a8171f1f3649b3d61a17d59aaa634e4892328c3f
Michał Skrzypczyński <justflow555@gmail.com>
2020-07-01 19:51:34 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <div>Hello</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+};
+</script>
+
+<style>
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
